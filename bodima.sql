790500aa413d4de55420d602dde428d6bfc35c68
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2020-11-24 07:40:51 +0530

    multiple search feature added

--- bodima.sql
+++ bodima.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 23, 2020 at 01:53 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.3
+-- Generation Time: Nov 24, 2020 at 03:09 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -528,6 +527,7 @@ ALTER TABLE `boardings_owner`
 ALTER TABLE `boarding_post`
   ADD PRIMARY KEY (`B_post_id`),
   ADD KEY `BOid` (`BOid`);
+ALTER TABLE `boarding_post` ADD FULLTEXT KEY `category` (`category`,`girlsBoys`,`image`,`house_num`,`lane`,`city`,`district`,`description`);
 
 --
 -- Indexes for table `complain_boarding`
